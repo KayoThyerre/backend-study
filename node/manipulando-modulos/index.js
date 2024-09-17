977eda96929c1e816bcffaeae7debc1cdef58e84
Kayo Thyerre <kayothyerrework@gmail.com>
2024-09-17 01:27:18 -0300

    nodejs study

---
+++ node/manipulando-modulos/index.js
@@ -0,0 +1,20 @@
+const { error } = require('console');
+const fs = require('fs');
+
+fs.readFile('meu-arquivo.txt', 'utf-8', (err, data) => {
+    if(err) throw err;
+    console.log(data);
+});
+
+const users = [
+    { name: 'Ricardo', age: 30 },
+    { name: 'Maria', age: 45 },
+ ];
+ 
+ fs.writeFile('users.txt', JSON.stringify(users), {
+    encoding: 'utf-8', 
+    flag: 'a'
+ }, (err) => {
+    if (err) throw err;
+    console.log('escrito com sucesso');
+ });
