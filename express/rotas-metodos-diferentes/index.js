ee7f2eb295f438137395cb052500deb25e0365d6
Kayo Thyerre <kayothyerrework@gmail.com>
2024-09-28 12:30:51 -0300

    routes | chain rep | middlewares begin

---
+++ express/rotas-metodos-diferentes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/users/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Este é o perfil do usuário: ${name}`);
+});
+
+app.put('/users/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Este é o perfil do usuário ${name} usando o PUT`);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor em execução em http://localhost:3000');
+});
